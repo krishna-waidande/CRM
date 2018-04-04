3525fa7ff0d79143644cd4d9a575099b7895bda2
krishna-waidande <krishnawaidande105@gmail.com>
2018-04-04 14:57:24 +0530

    updated JiraTicket.java

--- src/main/java/com/krishagni/crm/domain/JiraTicket.java
+++ src/main/java/com/krishagni/crm/domain/JiraTicket.java
@@ -1,11 +1,25 @@
 package com.krishagni.crm.domain;
 
-public class Ticket {
+public class JiraTicket {
+	private int id;
+	
 	private String jiraId;
 	
 	private String resolveDate;
 	
 	private String summary;
+	
+	private Company company;
+	
+	private int creditsUsed;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getJiraId() {
 		return jiraId;
@@ -29,5 +43,21 @@ public class Ticket {
 
 	public void setSummary(String summary) {
 		this.summary = summary;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+	public int getCreditsUsed() {
+		return creditsUsed;
+	}
+
+	public void setCreditsUsed(int creditsUsed) {
+		this.creditsUsed = creditsUsed;
 	}
 }
